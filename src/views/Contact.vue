4efb3d9a7ae913ab4828a0a6413126ac691aac15
xiang yi <xiangyisunn@gmail.com>
2021-07-22 22:19:46 +0200

    contact form complete

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -7,36 +7,37 @@
       </p>
     </div>
     <form action="" class="contact_info" autocomplete="off">
-      <div class="section">
-        <input type="text" name="name" v-model="form.name" required />
-        <label for="name" class="label_name">
-          <span class="content_name">Name</span>
-        </label>
+      <div class="input_box">
+        <input
+          type="text"
+          name="name"
+          v-model="form.name"
+          required
+          autocomplete="off"
+        />
+        <label for="name" class="label_name"> Name </label>
       </div>
 
-      <div class="section">
-        <input type="email" name="email" v-model="form.email" required />
-        <label for="email" class="label_name">
-          <span class="content_name">Email</span>
-        </label>
+      <div class="input_box">
+        <input
+          type="email"
+          name="email"
+          v-model="form.email"
+          required
+          autocomplete="off"
+        />
+        <label for="email"> Email </label>
       </div>
 
-      <div class="section">
-        <input type="text" name="message" v-model="form.message" required />
-        <label for="message" class="label_name">
-          <span class="content_name">Message</span>
-        </label>
+      <div class="input_box">
+        <textarea
+          v-model="form.message"
+          name="message"
+          required
+          autocomplete="off"
+        />
+        <label for="message"> Message </label>
       </div>
-
-      <!-- <label for="email">
-        <input type="email" name="email" v-model="form.email" required />
-        <span class="content_name">Email</span>
-      </label> -->
-
-      <!-- <label for="message">
-        <textarea v-model="form.message" name="message" required />
-        <span class="content_name">Message</span>
-      </label> -->
 
       <BaseButton type="submit" @submit.prevent="sendForm"></BaseButton>
     </form>
@@ -80,92 +81,95 @@ export default {
 </script>
 
 <style scoped>
-/* * {
+* {
   transition: all ease-in 0.3s;
-} */
+}
 .contact_page {
   width: 40%;
   margin-top: 10vh;
   padding: 1rem;
-
   display: flex;
   flex-direction: column;
   align-items: center;
+  /* overflow: hidden; */
 }
-
 #title {
   width: 100%;
   margin: 1.25rem;
   text-align: center;
 }
-
 form {
   width: 80%;
   padding: 1rem;
 }
-
-.section {
-  width: 100%;
-  height: 2.5rem;
-  position: relative;
-  margin-bottom: 2rem;
+input {
+  padding: 10px 0;
+  margin-bottom: 30px;
+}
+textarea {
+  resize: none;
+  height: 5rem;
+  padding: 10px 0;
+  margin-bottom: 40px;
+  font-family: "Roboto", sans-serif;
 }
 
-input {
+textarea,
+input[type="text"],
+input[type="email"] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border-radius: 0;
+}
+input,
+textarea {
   width: 100%;
-  height: 100%;
-  padding-top: 20px;
-  border: none;
   outline: none;
+  box-sizing: border-box;
+  box-shadow: none;
+  border: none;
+  border-bottom: 2px solid grey;
+}
+
+.input_box textarea {
+  margin-bottom: 20px;
+}
+.input_box {
+  position: relative;
 }
 
 label {
   position: absolute;
-  width: 100%;
-  height: 100%;
+  top: 10px;
   left: 0;
-  bottom: 0;
   pointer-events: none;
-  border-bottom: 1px solid rgb(46, 46, 46);
-  color: grey;
+  color: #999;
   font-size: 0.85rem;
 }
-label::after {
-  content: "";
-  width: 5%;
-  height: 100%;
-  position: absolute;
-  right: 0;
-  bottom: -1px;
-  border-bottom: 2px solid #44a7c3;
-  opacity: 0;
-}
 
-input:focus + .label_name::after,
-input:valid + .label_name::after {
-  width: 100%;
-  opacity: 1;
-  transition: width 0.2s ease-in;
-}
-.content_name {
-  position: absolute;
-  bottom: 5px;
+input:focus ~ label,
+input:valid ~ label,
+textarea:focus ~ label,
+textarea:valid ~ label {
+  top: -12px;
   left: 0;
-}
-
-input:focus + .label_name .content_name,
-input:valid + .label_name .content_name {
-  transform: translateY(-150%);
   font-size: 0.75rem;
   font-weight: 600;
-  color: #44a7c3;
-  transition: all 0.2s ease-in;
+  color: #3994ad;
 }
+
+input:focus,
+input:valid,
+textarea:focus,
+textarea:valid {
+  border-bottom: 2px solid #3994ad;
+}
+
 .contact_info {
   opacity: 0;
   transform: scale(0.9);
 }
-
 .info_text {
   margin-top: 1rem;
   font-weight: 500;
@@ -176,7 +180,6 @@ input:valid + .label_name .content_name {
   justify-content: center;
   align-items: center;
 }
-
 @media (max-width: 600px) {
   .contact_page {
     width: 90%;
